f83529043a2c08cf40b746328fa4f656c9e24dc0
Ric Janus Sapasap <rsapasap@77global.biz>
2019-04-07 04:23:22 +0800

    Implement quiz creation integration with iost

--- src/main/java/com/confetti/demo/service/IOSTService.java
+++ src/main/java/com/confetti/demo/service/IOSTService.java
@@ -1,52 +1,98 @@
 package com.confetti.demo.service;
 
+import com.confetti.demo.model.Choice;
+import com.confetti.demo.model.Question;
+import com.confetti.demo.model.Quiz;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import iost.Client;
 import iost.IOST;
 import iost.Keychain;
 import iost.crypto.Base58;
 import iost.crypto.Ed25519;
 import iost.crypto.KeyPair;
-import iost.model.account.Account;
 import iost.model.transaction.Transaction;
 import iost.model.transaction.TxReceipt;
+import org.bouncycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Service
 public class IOSTService {
-
-    private String URL = "192.168.50.23:30002";
-    private String accountName = "User";
-    private byte[] privateKey;
-
+    private static final String CONTRACT_ID = "ContractAimpWmcZ7LNiEmSXCktGGtAhRyAwuSjdC3WSeyJeykio";
     private Client client;
     private IOST iost;
     private Keychain account;
 
-    @Autowired
-    public IOSTService(Client client, IOST iost, Keychain account) {
-        this.client = client;
-        this.iost = iost;
-        this.account = account;
+    private ObjectMapper objectMapper;
 
-        this.client = new Client(this.URL);
+    @Autowired
+    public IOSTService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.client = new Client("http://localhost:30001/");
 
         try {
-            this.privateKey = Base58.decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1");
+            byte[] privateKeyBytes = Base58.decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1");
+            this.account = new Keychain("admin");
+            KeyPair kp = new Ed25519(privateKeyBytes);
+            account.addKey("owner", kp);
+            account.addKey("active", kp);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        this.account = new Keychain(accountName);
-        KeyPair kp = new Ed25519(privateKey);
-        account.addKey("owner", kp);
-        account.addKey("active", kp);
-
         this.iost = new IOST();
+    }
+
+    public void createQuiz(Quiz quiz) {
+        List<Map<String, String>> hashList = new ArrayList<>();
+
+        for (Question question : quiz.getQuestions()) {
+            Map<String, String> map = new HashMap<>();
+            map.put("questionHash", hashString(question.getQuestion()));
+            Choice correctChoice = question.getChoices().stream()
+                    .filter(Choice::isCorrect)
+                    .collect(Collectors.toList()).get(0);
+            map.put("answerHash", hashString(correctChoice.getDescription()));
+            hashList.add(map);
+        }
+
+        try {
+            Transaction tx = iost.callABI(CONTRACT_ID,
+                    "createQuiz",
+                    quiz.getId(),
+                    quiz.getQuizName(),
+                    quiz.getSponsor(),
+                    quiz.getPrizepool(),
+                    objectMapper.writeValueAsString(hashList));
+            tx.addApprove("iost", "10000");
+            account.publish(tx);
+            this.sendTxAndPrint(tx);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String hashString(String input) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        return new String(Hex.encode(hash));
     }
 
     private void sendTxAndPrint(Transaction tx) {
@@ -54,7 +100,6 @@ public class IOSTService {
         try {
             txHash = this.client.sendTx(tx);
         } catch (IOException e) {
-            System.out.println("");
             e.printStackTrace();
             return;
         }
@@ -71,20 +116,5 @@ public class IOSTService {
             System.out.println("ERROR，message:");
             e.printStackTrace();
         }
-    }
-
-    public Transaction callContract (String cid, String abi, Object data) {
-        return iost.callABI(cid,abi,data);
-    }
-
-    public void createQuiz(Object data) {
-        // make a call
-        Transaction tx = iost.callABI("Contract4BxHBkLoAqZfZLNJKiFPE722uDi9r4z7aAT9oaLWgRez", "createQuiz", data); // 智能合约，abi，抵押者，收益者，iost数量
-        tx.addApprove("iost", "10000");
-
-        // sign
-        account.publish(tx);
-
-        this.sendTxAndPrint(tx);
     }
 }
